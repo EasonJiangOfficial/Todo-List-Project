aaaae86f44bc59fb09b31c3e8989b9aab3ab4fe6
Eason Jiang <67925354+EasonJiangOfficial@users.noreply.github.com>
2022-09-03 23:49:30 -0500

    Adding basic files

---
+++ index.js
@@ -0,0 +1,7 @@
+let addButton = document.getElementById("add_button")
+let itemModal = document.getElementById("item_modal")
+let listItems = []
+
+let addItem = () =>{
+    itemModal.classList.toggle("show")
+    }
